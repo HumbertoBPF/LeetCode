28738948087451e4f324bf97b14b893ebb2fe304
HumbertoBPF <humbertobpf@gmail.com>
2022-03-16 00:26:38 -0300

    Solution problem 53

--- src/SolutionLeetCode53.java
+++ src/SolutionLeetCode53.java
@@ -2,88 +2,26 @@
 public class SolutionLeetCode53 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] nums = {1,2,-1,-2,2,1,-2,1,4,-5,4};
+		int[] nums = {-2,-3,-1,-5};
 		System.out.println(maxSubArray(nums));
 	}
 
 	public static int maxSubArray(int[] nums) {
-		// The two pointers algorithm is going to be used with each pointer
-		// stating at one extremity of the array
-		int leftPointer = 0;
-		int rightPointer = nums.length-1;
-		// relativeSum: when an item is removed from [leftPointer,rightPointer],
-		// it is subtracted from this variable. Hence, this variable stands for
-		// the relative effect that this remotion has over the sum of the items
-		// of the array, whichever it is. 
-		int relativeSum = 0; 
-		// We are interested in the lowest relative sum, because at the end of 
-		// the code, some value of relativeSum variable has to be summed to the
-		// actual sum so as to maximize the absoluteSum.
-		int maxRelativeSum = 0;
-		// It is the real sum of the array items. It is going to be computed while
-		// the array is iterated by the pointers.
-		int absoluteSum = 0;
+		// Solution using Kadane's algorithm
+		// Maximum sum considering the subarrays ended with the current item of the for each loop
+		int localMaximum = nums[0];
+		// Maximum sum considering all the subarrays seen so far
+		int globalMaximum = nums[0];
 		
-		// Iteration of the pointers through the array.
-		while (Math.abs(rightPointer-leftPointer) != 0) {
-			// In order to decide which pointer(s) will move, we compare the values pointed by them. Because
-			// the value of the moving pointer is removed from [leftPointer,rightPointer], we move the pointer
-			// pointing to the lowest value.
-			if (nums[leftPointer] < nums[rightPointer]) {
-				relativeSum -= nums[leftPointer];
-				absoluteSum += nums[leftPointer];
-				leftPointer++;
-			}else if (nums[leftPointer] > nums[rightPointer]) {
-				relativeSum -= nums[rightPointer];
-				absoluteSum += nums[rightPointer];
-				rightPointer--;
-			// If the pointed values are equal, we look to the item to be pointed after moving both pointers.
-			// For this case, we move the pointer that will pointer to the lowest item.
-			}else {
-				if (Math.abs(rightPointer-leftPointer) > 1) {
-					if (nums[leftPointer+1] < nums[rightPointer-1]) {
-						relativeSum -= nums[leftPointer];
-						absoluteSum += nums[leftPointer];
-						leftPointer++;
-					}else if (nums[leftPointer+1] > nums[rightPointer-1]) {
-						relativeSum -= nums[rightPointer];
-						absoluteSum += nums[rightPointer];
-						rightPointer--;
-					// If both pointers are going to point to the same value (but hold by 
-					// different items), we move both pointers. 
-					}else {
-						relativeSum -= nums[leftPointer];
-						absoluteSum += nums[leftPointer];
-						leftPointer++;
-						// Updates the maxRelativeSum here, because two moves are going to be performed
-						if (maxRelativeSum < relativeSum) {
-							maxRelativeSum = relativeSum;
-						}
-						relativeSum -= nums[rightPointer];
-						absoluteSum += nums[rightPointer];
-						rightPointer--;
-					}
-				//If both of them are going to point to the same item(there is only one 
-				//item remaining in the interval when both move), we move anyone of them.
-				}else {
-					relativeSum -= nums[rightPointer];
-					absoluteSum += nums[rightPointer];
-					rightPointer--;
-				}
-			}
-			// Updates the maxRelativeSum
-			if (maxRelativeSum < relativeSum) {
-				maxRelativeSum = relativeSum;
-			}
-			System.out.println("maxRelativeSum = "+maxRelativeSum);
-			System.out.println("relativeSum = "+relativeSum);
+		for (int i = 1;i<nums.length;i++) {
+			// The local maximum will be always between the array composed of only the current item
+			// and the subarray correspondent to the last local maximum appended of the current item
+			localMaximum = Math.max(localMaximum+nums[i], nums[i]);
+			// Check if it is necessary to update the global maximum
+			globalMaximum = Math.max(localMaximum, globalMaximum);
 		}
 		
-		// Adds the last element to the absolute sum
-		absoluteSum += nums[rightPointer];
-		
-		return absoluteSum+maxRelativeSum;
+		return globalMaximum;
     }
 	
 }
